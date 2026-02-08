79934efa1943e946bc303e9a492f62b6e4616bb8
Shangda777 <guolinlin0725@gmail.com>
2026-02-08 18:53:47 +0800

    Complete A-Checkstyle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,35 @@
 plugins {
     application
     java
+    id("checkstyle")
 }
 
 repositories {
     mavenCentral()
 }
 
+checkstyle {
+    toolVersion = "11.0.0"
+}
+
+val javaFxVersion = "17.0.7"
+
 dependencies {
+    // JavaFX (multi-platform)
+    implementation("org.openjfx:javafx-base:$javaFxVersion:win")
+    implementation("org.openjfx:javafx-base:$javaFxVersion:mac")
+    implementation("org.openjfx:javafx-base:$javaFxVersion:linux")
+    implementation("org.openjfx:javafx-controls:$javaFxVersion:win")
+    implementation("org.openjfx:javafx-controls:$javaFxVersion:mac")
+    implementation("org.openjfx:javafx-controls:$javaFxVersion:linux")
+    implementation("org.openjfx:javafx-fxml:$javaFxVersion:win")
+    implementation("org.openjfx:javafx-fxml:$javaFxVersion:mac")
+    implementation("org.openjfx:javafx-fxml:$javaFxVersion:linux")
+    implementation("org.openjfx:javafx-graphics:$javaFxVersion:win")
+    implementation("org.openjfx:javafx-graphics:$javaFxVersion:mac")
+    implementation("org.openjfx:javafx-graphics:$javaFxVersion:linux")
+
+    // JUnit 5 for tests
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
